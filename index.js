d4e2b4df04a74fcd0310ab7cd0ebfec664adceee
alec <alec@petros.audio>
2017-12-05 20:57:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function johnLennonFacts(facts) {
   }
 return array
 }
+function iLoveTheBeatles(n) {
+  var i = [];
+  do {
+    i.push("I love the Beatles!");
+    console.log(n++);
+  } while (n < 15);
+  return i;
+}
